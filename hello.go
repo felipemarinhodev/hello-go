945f14912e18bd420f04af5b0a14af26165b4e40
felipemarinhodev <felipmarinho@gmail.com>
2022-05-20 07:52:42 -0700

    introdução ao go

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var nome string = "Felipe"
+	var idade int
+	var versao float32 = 1.1
+	fmt.Println("olá sr.", nome, "sua idade é", idade)
+	fmt.Println("este programa está na versão", versao)
+}
