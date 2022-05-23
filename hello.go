15f4695492063d797827e966b4561065812989cf
felipemarinhodev <felipmarinho@gmail.com>
2022-05-23 09:39:59 -0700

    dividindo o codigo em funções

--- hello.go
+++ hello.go
@@ -2,22 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main()  {
-	name := "Felipe"
-	version := 1.1
-	fmt.Println("olá sr.", name)
-	fmt.Println("este programa está na versão", version)
+	showIntro()
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do programa.")
+	showMenu()
 
-	var command int;
-	fmt.Scan(&command)
-
-	fmt.Println("O comando escolhido foi", command)
+	command := readCommand()
 
 	switch command {
 		case 1:
@@ -26,7 +19,30 @@ func main()  {
 			fmt.Println("Exibindo Logs...")
 		case 0:
 			fmt.Println("Saindo do programa...")
+			os.Exit(0)
 		default:
 			fmt.Println("Comando não suportado.")
+			os.Exit(-1)
 	}
+}
+
+func showMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa.")
+}
+
+func showIntro() {
+	name := "Felipe"
+	version := 1.1
+	fmt.Println("olá sr.", name)
+	fmt.Println("este programa está na versão", version)
+}
+
+func readCommand() int {
+	var commandRead int;
+	fmt.Scan(&commandRead)
+
+	fmt.Println("O comando escolhido foi", commandRead)
+	return commandRead
 }
