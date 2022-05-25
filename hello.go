6486b59357ecb000a17114c9746d5a3d0c43f605
felipemarinhodev <felipmarinho@gmail.com>
2022-05-24 18:16:45 -0700

    exibindo os logs registrados.

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -17,7 +18,6 @@ const monitoring = 3
 func main()  {
 	showIntro()
 
-	registerLog("site-false", false)
 	for {
 		showMenu()
 
@@ -28,6 +28,7 @@ func main()  {
 				startMonitoring()
 			case 2:
 				fmt.Println("Exibindo Logs...")
+				printLogs()
 			case 0:
 				fmt.Println("Saindo do programa...")
 				os.Exit(0)
@@ -123,4 +124,14 @@ func registerLog(site string, status bool) {
 	}
 	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: "+ strconv.FormatBool(status) + "\n")
 	file.Close()
+}
+
+func printLogs() {
+
+	file, err  := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(file))
 }
